353d6dcc92c9c347ec1c51b5dbe0533b3bdac033
blumikon98766 <marcinzaraj@gmail.com>
2017-12-17 15:39:30 +0100

    create list of cinemas

--- src/main/java/pl/com/bottega/cms/ui/CinemaController.java
+++ src/main/java/pl/com/bottega/cms/ui/CinemaController.java
@@ -1,21 +1,33 @@
 package pl.com.bottega.cms.ui;
 
 import org.springframework.web.bind.annotation.*;
+import pl.com.bottega.cms.application.CinemaDto;
+import pl.com.bottega.cms.application.CinemaFinder;
 import pl.com.bottega.cms.application.CommandGateway;
 import pl.com.bottega.cms.model.commands.CreateCinemaCommand;
 
+import java.util.List;
+
 
 @RestController
-
 public class CinemaController {
-    private CommandGateway commandGateway;
 
-    public CinemaController(CommandGateway commandGateway) {
+    private CommandGateway commandGateway;
+    private CinemaFinder cinemaFinder;
+
+    public CinemaController(CommandGateway commandGateway, CinemaFinder cinemaFinder) {
         this.commandGateway = commandGateway;
+        this.cinemaFinder = cinemaFinder;
     }
 
     @PutMapping("/cinemas")
-    public void create(@RequestBody CreateCinemaCommand cmd){
+    public void create(@RequestBody CreateCinemaCommand cmd) {
         commandGateway.execute(cmd);
+    }
+
+    @GetMapping("/cinemas")
+    public List<CinemaDto> getAll() {
+        List<CinemaDto> resultList = cinemaFinder.getAll();
+        return resultList;
     }
 }
