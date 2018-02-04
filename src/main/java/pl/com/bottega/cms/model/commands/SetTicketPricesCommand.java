ec669dec38c4673fc1cf6f023ea35dddc364004e
Marcin <marcinzaraj@gmail.com>
2018-02-04 13:24:43 +0100

    As an admin I want to define shows pricing - all done

--- src/main/java/pl/com/bottega/cms/model/commands/SetTicketPricesCommand.java
+++ src/main/java/pl/com/bottega/cms/model/commands/SetTicketPricesCommand.java
@@ -1,25 +1,35 @@
 package pl.com.bottega.cms.model.commands;
 
 import java.math.BigDecimal;
+import java.rmi.MarshalledObject;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class SetTicketPricesCommand implements Command {
 
     private Long movieId;
     private Map<String,BigDecimal> prices;
 
-
     public void validate(ValidationErrors errors){
+
+
         for (Map.Entry<String,BigDecimal> entry : prices.entrySet()){
-            if(entry.getValue().compareTo(BigDecimal.ZERO) == -1 )
-            errors.add("prices", "Prices can not be under 0");
+            if(entry.getValue() == null || entry.getValue().compareTo(BigDecimal.ZERO) == -1)
+            errors.add("prices", "Prices must be positive");
         }
 
             if(prices!= null && !( prices.containsKey("regular") ))
-            errors.add("prices", "Must be regular ticker");
+            errors.add("prices", "Must be regular ticket");
 
             if(prices != null && !( prices.containsKey("student") ))
-            errors.add("prices", "Must be student ticker");
+            errors.add("prices", "Must be student ticket");
+
+
+        for (Map.Entry<String,BigDecimal> entry : prices.entrySet()){
+            if(entry.getKey().trim().isEmpty())
+                errors.add("ticket Type", "Can not be empty");
+        }
+
     }
 
     public Long getMovieId() {
