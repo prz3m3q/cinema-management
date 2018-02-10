990883679c63367f5a3fae537504db5b133949f0
przemek <prz3m3q@wp.pl>
2018-02-10 16:37:20 +0100

    poprawna walidacja dni tygodnia

--- src/main/java/pl/com/bottega/cms/model/commands/Command.java
+++ src/main/java/pl/com/bottega/cms/model/commands/Command.java
@@ -1,7 +1,14 @@
 package pl.com.bottega.cms.model.commands;
 
+import pl.com.bottega.cms.model.Ticket;
+
 import java.text.DateFormatSymbols;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import static pl.com.bottega.cms.model.Customer_.email;
 
 public interface Command {
 
@@ -18,6 +25,15 @@ public interface Command {
     default void validatePresence(ValidationErrors errors, String field, Object value) {
         if (value == null) {
             errors.add(field, "can't be blank");
+        }
+    }
+
+    default void validateEmail(ValidationErrors errors, String field, String value) {
+        String regex = "^([_a-zA-Z0-9-]+(\\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*(\\.[a-zA-Z]{1,6}))?$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(value);
+        if (!matcher.matches()) {
+            errors.add(field, "wrong email format");
         }
     }
 
@@ -48,8 +64,18 @@ public interface Command {
             errors.add(field, "can't be blank");
         }
         String[] weekdays = DateFormatSymbols.getInstance(Locale.US).getWeekdays();
-        if (value != null && !Arrays.stream(weekdays).anyMatch(day -> value.contains(day))) {
+        if (value != null && value.stream().anyMatch(day -> !Arrays.asList(weekdays).contains(day))) {
             errors.add(field, "some name of weekday is wrong");
+        }
+    }
+
+    default void validateUniqueTicketsKind(ValidationErrors errors, String field, Set<Ticket> value) {
+        if (value == null) {
+            errors.add(field, "can't be blank");
+        }
+        List<String> unique = value.stream().map(ticket -> ticket.getKind()).collect(Collectors.toList());
+        if (value != null && value.size() != unique.size()) {
+            errors.add(field, "tickets is not unique");
         }
     }
 
